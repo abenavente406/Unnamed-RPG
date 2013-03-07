1988591b0238465851785147729b5a0134c6d7a4
abenavente406 <abenavente406@gmail.com>
2013-03-06 22:34:22 -0500

    Prepared branch for experimenting.

---
+++ UnnamedRpg/UnnamedRpgContent/Levels/Castle_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Castle_2" tilewidth="32" tileheight="32">
+ <image source="TileSheets/Castle_2.png" trans="ffffff" width="512" height="512"/>
+</tileset>
